eff3bd854d9d9a1998233a3e2362913d4f49cd8f
dondrzzy <sibo.donald16@gmail.com>
2018-07-06 15:04:29 +0300

    Update README.md on master

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,157 +1,148 @@
-import dispatcher from "../dispatcher";
+import axios from 'axios';
+import dispatcher from '../dispatcher';
 import * as urlConfig from '../utils/baseUrl';
-import axios from "axios";
-const baseUrl = urlConfig.default.getBaseUrl()+'auth/';
 
-export const authenticateUser = data =>{
-    axios.post(
-        baseUrl+'login',
-        data,
-        {
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        })
-    .then(response => {
-        dispatcher.dispatch({
-            type:"LOGIN_USER",
-            data:response.data
-        })
+const baseUrl = `${urlConfig.default.getBaseUrl()}auth/`;
+
+export const authenticateUser = (data) => {
+  axios
+    .post(`${baseUrl}login`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
-    .catch( error => {
-        if(error.response){
-            dispatcher.dispatch({
-                type:"LOGIN_USER",
-                data: error.response.data
-            });
-        }
+    .then((response) => {
+      dispatcher.dispatch({
+        type: 'LOGIN_USER',
+        data: response.data,
+      });
+    })
+    .catch((error) => {
+      if (error.response) {
+        dispatcher.dispatch({
+          type: 'LOGIN_USER',
+          data: error.response.data,
+        });
+      }
     });
-}
+};
 
-export const logout = () =>{
-    const token = window.localStorage.getItem('jwt') 
-    ? JSON.parse(window.localStorage.getItem('jwt')) 
+export const logout = () => {
+  const token = window.localStorage.getItem('jwt')
+    ? JSON.parse(window.localStorage.getItem('jwt'))
     : null;
-    axios.get(
-        baseUrl+'logout',
-        {
-            headers: {
-            'Content-Type': 'application/json',
-            'x-access-token': token
-            }
+  axios
+    .get(`${baseUrl}logout`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-access-token': token,
+      },
     })
-    .then( response => {
+    .then((response) => {
+      dispatcher.dispatch({
+        type: 'LOGOUT_USER',
+        data: response.data,
+      });
+    })
+    .catch((error) => {
+      if (error.response) {
         dispatcher.dispatch({
-            type:"LOGOUT_USER",
-            data:response.data
+          type: 'LOGOUT_USER',
+          data: error.response.data,
         });
-    })
-    .catch( error => {
-        if(error.response){
-            dispatcher.dispatch({
-                type:"LOGOUT_USER",
-                data: error.response.data
-            });
-        }
+      }
     });
-}
+};
 
-export const registerUser = data => {
-    axios.post(
-        baseUrl+'register',
-        data,
-        {
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        })
-    .then( response => {
-        dispatcher.dispatch({
-            type:"REGISTERED_USER",
-            data:response.data
-        });
+export const registerUser = (data) => {
+  axios
+    .post(`${baseUrl}register`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
-    .catch( error => {
-        if(error.response){
-            dispatcher.dispatch({
-                type:"REGISTERED_USER",
-                data: error.response.data
-            });
-        }
+    .then((response) => {
+      dispatcher.dispatch({
+        type: 'REGISTERED_USER',
+        data: response.data,
+      });
+    })
+    .catch((error) => {
+      if (error.response) {
+        dispatcher.dispatch({
+          type: 'REGISTERED_USER',
+          data: error.response.data,
+        });
+      }
     });
-    
-}
+};
 
-export const resetPassword = data =>{
-    axios.post(
-        baseUrl+'reset-password',
-        data,
-        {
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        })
-    .then(response => {
-        dispatcher.dispatch({
-            type:"RESET_PASSWORD",
-            data:response.data
-        });
+export const resetPassword = (data) => {
+  axios
+    .post(`${baseUrl}reset-password`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
-    .catch( error => {
-        if(error.response){
-            dispatcher.dispatch({
-                type:"RESET_PASSWORD",
-                data: error.response.data
-            });
-        }
-    });
-}
-export const forgotPassword = email =>{
-    let data ={ "email" : email }
-    axios.post(
-        baseUrl+'forgot-password',
-        data,
-        {
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        })
-    .then( response => {
-        dispatcher.dispatch({
-            type:"FORGOT_PASSWORD",
-            data:response.data
-        });
+    .then((response) => {
+      dispatcher.dispatch({
+        type: 'RESET_PASSWORD',
+        data: response.data,
+      });
     })
-    .catch( function(error){
-        if(error.response)
-            dispatcher.dispatch({
-                type:"FORGOT_PASSWORD",
-                data: error.response.data
-            });
+    .catch((error) => {
+      if (error.response) {
+        dispatcher.dispatch({
+          type: 'RESET_PASSWORD',
+          data: error.response.data,
+        });
+      }
     });
-}
+};
+export const forgotPassword = (email) => {
+  const data = { email };
+  axios
+    .post(`${baseUrl}forgot-password`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      dispatcher.dispatch({
+        type: 'FORGOT_PASSWORD',
+        data: response.data,
+      });
+    })
+    .catch((error) => {
+      if (error.response) {
+        dispatcher.dispatch({
+          type: 'FORGOT_PASSWORD',
+          data: error.response.data,
+        });
+      }
+    });
+};
 
-export const verifyToken = token =>{
-    let data ={ "token" : token }
-    axios.post(
-        baseUrl+'verify-password-token',
-        data,
-        {
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        })
-    .then(function (response) {
+export const verifyToken = (token) => {
+  const data = { token };
+  axios
+    .post(`${baseUrl}verify-password-token`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      dispatcher.dispatch({
+        type: 'SHOW_RESET_PASSWORD',
+        data: response.data,
+      });
+    })
+    .catch((error) => {
+      if (error.response) {
         dispatcher.dispatch({
-            type:"SHOW_RESET_PASSWORD",
-            data:response.data
+          type: 'SHOW_RESET_PASSWORD',
+          data: error.response.data,
         });
-    }).catch(function (error) {
-        if(error.response){
-            dispatcher.dispatch({
-                type:"SHOW_RESET_PASSWORD",
-                data: error.response.data
-            });
-        }
+      }
     });
-}
+};
--- src/components/reviews/ReviewItem.js
+++ src/components/reviews/ReviewItem.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 
-export default class ReviewItem extends Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return (
-            <li >
-                <b>{this.props.review.user} : </b>
-                {this.props.review.text}
-                <br/>
-                <p className="badge badge-danger">{this.props.review.created_at}</p>
-                <span className="mini-border-bottom"></span>
-            </li>
-        )
-    }
-} 
+export default class ReviewItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li>
+        <b>
+          {this.props.review.user}
+          {' '}
+:
+          {' '}
+        </b>
+        {this.props.review.text}
+        <br />
+        <p className="badge badge-danger">
+          {this.props.review.created_at}
+        </p>
+        <span className="mini-border-bottom" />
+      </li>
+    );
+  }
+}
